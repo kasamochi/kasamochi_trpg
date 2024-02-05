d88e8a4104b6f38e3ac381fe9f04b8c66bfe97c5
kasamochi <101503312+kasamochi@users.noreply.github.com>
2024-02-05 17:25:22 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // JavaScript Document
 //$(function(){
 //	
@@ -118,3 +120,80 @@ $(function () {
  $('#currentDate').text(y + "/" + m + "/" + d);
  $('#currentDate-2').text(y + "/" + m + "/" + d);
 });
+
+
+window.addEventListener('load', function () {
+	let column_no = 0; //今回クリックされた列番号
+	let column_no_prev = 0; //前回クリックされた列番号
+	document.querySelectorAll('#sort_table_1 th,#sort_table_2 th,#sort_table_3 th,#sort_table_4 th,#sort_table_5 th,#sort_table_6 th,#sort_table_7 th,#sort_table_8 th,#sort_table_9 th,#sort_table_10 th,#sort_table_11 th,#sort_table_12 th,#sort_table_13 th,#sort_table_14 th,#sort_table_15 th').forEach(elm => {
+		elm.onclick = function () {
+			column_no = this.cellIndex; //クリックされた列番号
+			let table = this.parentNode.parentNode.parentNode;
+			let sortType = 0; //0:数値 1:文字
+			let sortArray = new Array; //クリックした列のデータを全て格納する配列
+			for (let r = 1; r < table.rows.length; r++) {
+				//行番号と値を配列に格納
+				let column = new Object;
+				column.row = table.rows[r];
+				column.value = table.rows[r].cells[column_no].textContent;
+				sortArray.push(column);
+				//数値判定
+				if (isNaN(Number(column.value))) {
+					sortType = 1; //値が数値変換できなかった場合は文字列ソート
+				}
+			}
+			if (sortType == 0) { //数値ソート
+				if (column_no_prev == column_no) { //同じ列が2回クリックされた場合は降順ソート
+					sortArray.sort(compareNumberDesc);
+				} else {
+					sortArray.sort(compareNumber);
+				}
+			} else { //文字列ソート
+				if (column_no_prev == column_no) { //同じ列が2回クリックされた場合は降順ソート
+					sortArray.sort(compareStringDesc);
+				} else {
+					sortArray.sort(compareString);
+				}
+			}
+			//ソート後のTRオブジェクトを順番にtbodyへ追加（移動）
+			let tbody = this.parentNode.parentNode;
+			for (let i = 0; i < sortArray.length; i++) {
+				tbody.appendChild(sortArray[i].row);
+			}
+			//昇順／降順ソート切り替えのために列番号を保存
+			if (column_no_prev == column_no) {
+				column_no_prev = -1; //降順ソート
+			} else {
+				column_no_prev = column_no;
+			}
+		};
+	});
+});
+//数値ソート（昇順）
+function compareNumber(a, b)
+{
+	return a.value - b.value;
+}
+//数値ソート（降順）
+function compareNumberDesc(a, b)
+{
+	return b.value - a.value;
+}
+//文字列ソート（昇順）
+function compareString(a, b) {
+	if (a.value < b.value) {
+		return -1;
+	} else {
+		return 1;
+	}
+	return 0;
+}
+//文字列ソート（降順）
+function compareStringDesc(a, b) {
+	if (a.value > b.value) {
+		return -1;
+	} else {
+		return 1;
+	}
+	return 0;
+}
